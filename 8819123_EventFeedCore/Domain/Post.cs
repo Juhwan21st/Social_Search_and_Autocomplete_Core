19c07c036436ed7d8605706d8e3465f194513766
Juhwan21st <jseo9123@conestogac.on.ca>
2025-12-13 13:59:47 -0500

    feat(domain): implement Post domain model
    - Add Post class with Id, Author, Content properties
    - Implement Likes counter with AddLike() method
    - Add IsPinned flag for pin functionality
    - Follow instruction requirements for feed display

---
+++ 8819123_EventFeedCore/Domain/Post.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventFeedCore.Domain
+{
+	public class Post
+	{
+		public int Id { get; }
+		public string Author { get; }
+		public string Content { get; }
+		public int Likes { get; private set; }
+		public bool IsPinned { get; set; }
+
+		public Post(int id, string author, string content)
+		{
+			Id = id;
+			Author = author;
+			Content = content;
+			Likes = 0;
+			IsPinned = false;
+		}
+
+		public void AddLike() => Likes++;
+	}
+}
