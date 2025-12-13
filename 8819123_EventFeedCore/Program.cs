9cc1ccc3c8b0af5287d702299553bb07e3c7ccb3
Juhwan21st <jseo9123@conestogac.on.ca>
2025-12-13 13:53:06 -0500

    feat: add interactive menu structure for event feed operations
    - Implement main loop with isRunning flag
    - Add 8 menu options (load, view, process, display, pin, delete, undo, redo)
    - Add exit option (0)
    - Placeholder TODOs for each feature implementation

--- 8819123_EventFeedCore/Program.cs
+++ 8819123_EventFeedCore/Program.cs
@@ -1,3 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿internal static class Program
+{
+    private static void Main()
+    {
+        bool isRunning = true;
 
-Console.WriteLine("Hello, World!");
+        while (isRunning)
+        {
+            Console.WriteLine("\n=== Event Feed & Moderation Core ===");
+            Console.WriteLine("1. Load events from file");
+            Console.WriteLine("2. View ingest queue");
+            Console.WriteLine("3. Process next event");
+            Console.WriteLine("4. Display feed");
+            Console.WriteLine("5. Pin a post");
+            Console.WriteLine("6. Delete a post");
+            Console.WriteLine("7. Undo");
+            Console.WriteLine("8. Redo");
+            Console.WriteLine("0. Exit");
+            Console.Write("> ");
+            
+            string input = Console.ReadLine();
+            
+            switch (input)
+            {
+                case "1":
+                    Console.WriteLine("TODO: Load events");
+                    break;
+                case "2":
+                    Console.WriteLine("TODO: View queue");
+                    break;
+                case "3":
+                    Console.WriteLine("TODO: Process event");
+                    break;
+                case "4":
+                    Console.WriteLine("TODO: Display feed");
+                    break;
+                case "5":
+                    Console.WriteLine("TODO: Pin post");
+                    break;
+                case "6":
+                    Console.WriteLine("TODO: Delete post");
+                    break;
+                case "7":
+                    Console.WriteLine("TODO: Undo");
+                    break;
+                case "8":
+                    Console.WriteLine("TODO: Redo");
+                    break;
+                case "0":
+                    isRunning = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option");
+                    break;
+            }
+        }
+    }
+}
